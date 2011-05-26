416df56bc19b9258984d2801296ee394f78a5099
Raimond Spekking <raymond@users.mediawiki.org>
2011-05-26 20:54:58 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-05-26 20:23:00 UTC)

--- ShortUrl.i18n.php
+++ ShortUrl.i18n.php
@@ -22,3 +22,102 @@ $messages['en'] = array(
 	'shorturl-toolbox-text' => 'Short URL'
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'shorturl' => 'Kurz-URL',
+	'shorturl-desc' => 'Ergänzt eine [[Special:ShortUrl|Spezialseite]] zum Erstellen von Kurz-URLs für Weiterleitungen',
+	'shorturl-not-found' => 'Die gesuchte Kurz-URL wurde leider nicht gefunden (Es ist keine Kurz-URL mit der Kennung $1 vorhanden)',
+	'shorturl-toolbox-title' => 'Kurz-URL kopieren, um sie zu nutzen',
+	'shorturl-toolbox-text' => 'Kurz-URL',
+);
+
+/** British English (British English)
+ * @author Surya Prakash.S.A.
+ */
+$messages['en-gb'] = array(
+	'shorturl' => 'குறுந்தொடுப்பு',
+	'shorturl-desc' => '[[Special:ShortUrl|வழிமாற்றுகளுக்கான குறுந்தொடுப்பு]]',
+	'shorturl-not-found' => 'மன்னிக்கவும், நீங்கள் எதிர்பார்க்கும் உரலி கிடைக்கவில்லை ($1 என்ற அடையாளத்துடன் கூடிய குறுந்தொடுப்பு ஏதுமில்லை)',
+	'shorturl-toolbox-title' => 'பகிர்வதற்காக இக்குறுந்தொடுப்பை நகலெடுக்கவும்',
+	'shorturl-toolbox-text' => 'குறுந்தொடுப்பு',
+);
+
+/** Galician (Galego)
+ * @author Toliño
+ */
+$messages['gl'] = array(
+	'shorturl' => 'Enderezo URL curto',
+	'shorturl-desc' => '[[Special:ShortUrl|Enderezo URL curto para as redireccións]]',
+	'shorturl-not-found' => 'Sentímolo, non se atopou o enderezo URL que está a buscar (non existe ningún enderezo URL curto co ID $1)',
+	'shorturl-toolbox-title' => 'Copie esta ligazón curta para compartir',
+	'shorturl-toolbox-text' => 'Enderezo URL curto',
+);
+
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'shorturl' => 'URL curte',
+	'shorturl-desc' => '[[Special:ShortUrl|URL curte pro redirectiones]]',
+	'shorturl-not-found' => 'Le URL que tu cerca non ha essite trovate (nulle URL curte con le ID $1 existe).',
+	'shorturl-toolbox-title' => 'Copiar iste ligamine curte pro divulgation',
+	'shorturl-toolbox-text' => 'URL curte',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'shorturl' => 'Кратка URL-адреса',
+	'shorturl-desc' => '[[Special:ShortUrl|Кратка URL-адреса за пренасочувања]]',
+	'shorturl-not-found' => 'Нажалост, не ја најдов URL-адресата што ја барате (не постои кратка URL-адреса со назнаката $1)',
+	'shorturl-toolbox-title' => 'Ископирајте ја оваа кратка врска за да ја споделите',
+	'shorturl-toolbox-text' => 'Кратка URL-адреса',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ */
+$messages['nl'] = array(
+	'shorturl' => 'Korte URL',
+	'shorturl-desc' => '[[Special:ShortUrl|Korte URL voor doorverwijzingen]]',
+	'shorturl-not-found' => 'Sorry, de URL waarnaar u zoekt is niet gevonden (er bestaat geen korte URL met ID $1)',
+	'shorturl-toolbox-title' => 'Kopieer deze korte link om te delen',
+	'shorturl-toolbox-text' => 'Korte URL',
+);
+
+/** Oriya (ଓଡ଼ିଆ)
+ * @author Psubhashish
+ */
+$messages['or'] = array(
+	'shorturl' => 'ଛୋଟ URL',
+	'shorturl-desc' => '[[Special:ShortUrl|ଆଉଥରେ ଫେରନ୍ତା ଲିଙ୍କପାଇଁ ଛୋଟ URL]]',
+	'shorturl-not-found' => 'କ୍ଷମା କରିବେ, ଆପଣ ଦେଇଥିବା URLଟି  ମିଳିଲା ନାହିଁ  ($1 ନାଆଁରେ କିଛି ଛୋଟ  URL ମିଳିଲା ନାହିଁ)',
+	'shorturl-toolbox-title' => 'ଏହି ଛୋଟ ଲିଙ୍କଟି ବାଣ୍ଟିବା ପାଇଁ ନକଲ କରନ୍ତୁ',
+	'shorturl-toolbox-text' => 'ଛୋଟ URL',
+);
+
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'shorturl' => 'URL Curta',
+	'shorturl-desc' => '[[Special:ShortUrl|URL Curta para redireccionamentos]]',
+	'shorturl-not-found' => 'A URL que pretende não foi encontrada (não existe nenhuma URL Curta com a identificação $1)',
+	'shorturl-toolbox-title' => 'Copie este link curto para partilhar',
+	'shorturl-toolbox-text' => 'URL Curta',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'shorturl' => 'Короткая ссылка',
+	'shorturl-desc' => '[[Special:ShortUrl|Короткая ссылка для перенаправлений]]',
+	'shorturl-not-found' => 'К сожалению, указанный URL-адрес не найден (нет короткой ссылки с идентификатором $1)',
+	'shorturl-toolbox-title' => 'Скопируйте эту короткую ссылку',
+	'shorturl-toolbox-text' => 'Короткая ссылка',
+);
+
