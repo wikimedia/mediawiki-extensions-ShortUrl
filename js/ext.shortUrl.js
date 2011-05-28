3655b2952a5ff3ea82654dda29d6ffd70628adff
Yuvaraj Pandian T <yuvipanda@users.mediawiki.org>
2011-05-28 03:59:26 +0000

    Followup to make js conform to mw coding standards

--- js/ext.shortUrl.js
+++ js/ext.shortUrl.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    if( $("#t-shorturl").length ) {
+jQuery( function ( $ ) {    
+	if( $("#t-shorturl").length ) {
         var url = $("#t-shorturl a").attr("href");
         $("#firstHeading").append('<a class="title-shortlink" href="' + url + '">' + url + '</a>');
     }
