0f69b0201f80b4328597713a521bc1de790bae66
Yuvaraj Pandian T <yuvipanda@users.mediawiki.org>
2011-05-30 13:39:18 +0000

    Added alias file

---
+++ ShortUrl.alias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'ShortUrl' => array( 'ShortUrl' ),
+);
