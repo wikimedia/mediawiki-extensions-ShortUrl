3ed2e341c9b048f2f2cdb0dec0f86659322f1d98
Ori Livneh <ori@wikimedia.org>
2013-04-15 10:48:47 -0700

    jshint fixes
    Change-Id: Ide953be64e272a0a99ada4136dc635b674c74823

--- js/ext.shortUrl.js
+++ js/ext.shortUrl.js
@@ -1,17 +1,21 @@
-jQuery( function( $ ) {
+
+( function ( mw, $ ) {
 	'use strict';
 
-	if( $( '#t-shorturl' ).length ) {
-		var url = $( '#t-shorturl a' ).attr( 'href' );
-		/* Add protocol for proto-relative urls */
-		var protoNonRelative = ( new mw.Uri( url ) ).toString();
-		$( '#firstHeading' ).append( 
-			$( '<div class="title-shortlink-container"></div>')
-				.append( $( '<a>' )
-				.addClass( 'title-shortlink' )
-				.attr( 'href', url )
-				.text( protoNonRelative )
-			)
-		);
-	}
-});
+	$( function () {
+		if ( $( '#t-shorturl' ).length ) {
+			var url = $( '#t-shorturl a' ).attr( 'href' ),
+				/* Add protocol for proto-relative urls */
+				protoNonRelative = ( new mw.Uri( url ) ).toString();
+			$( '#firstHeading' ).append(
+				$( '<div class="title-shortlink-container"></div>')
+					.append( $( '<a>' )
+					.addClass( 'title-shortlink' )
+					.attr( 'href', url )
+					.text( protoNonRelative )
+				)
+			);
+		}
+	});
+
+} ( mediaWiki, jQuery ) );
